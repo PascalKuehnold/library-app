1dfa73d5c6527f676465fc14a178d1ac781bb3e3
PascalKuehnold <pascal.kuehnold@gmail.com>
2023-01-26 00:52:10 +0100

    added reviewrequest model for receiving data

---
+++ backend/src/main/java/de/pascalkuehnold/springbootlibrary/requestmodels/ReviewRequest.java
@@ -0,0 +1,18 @@
+package de.pascalkuehnold.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+import java.util.Optional;
+
+@Data
+public class ReviewRequest {
+
+    private double rating;
+
+    private Long bookId;
+
+    private Optional<String> reviewDescription;
+
+
+
+}
