0d55c3bb6aacbbd89eae7f2f8eadf234a4369665
PascalKuehnold <pascal.kuehnold@gmail.com>
2023-01-26 00:49:48 +0100

    added controller classes for http requests

---
+++ backend/src/main/java/de/pascalkuehnold/springbootlibrary/controller/BookController.java
@@ -0,0 +1,40 @@
+package de.pascalkuehnold.springbootlibrary.controller;
+
+import de.pascalkuehnold.springbootlibrary.entity.Book;
+import de.pascalkuehnold.springbootlibrary.service.BookService;
+import de.pascalkuehnold.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+
+    @GetMapping("/secure/currentloans/count")
+    public int currentLoansCount(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.currentLoandsCount(userEmail);
+    }
+
+    @GetMapping("/secure/ischeckedout/byuser")
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return bookService.checkoutBookByUser(userEmail, bookId);
+    }
+
+}
