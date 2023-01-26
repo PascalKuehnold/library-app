f40dcd34c8f2e1af8bdd395ba30437ccfb1b7ab0
PascalKuehnold <pascal.kuehnold@gmail.com>
2023-01-26 23:30:51 +0100

    added ReviewListPage to the routes

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -10,6 +10,7 @@ import { oktaConfig } from "./lib/octaConfig";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 import { LoginCallback, Security } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
+import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -43,6 +44,9 @@ export const App = () => {
             </Route>
             <Route path="/search">
               <SearchBooksPage />
+            </Route>
+            <Route path="/reviewlist/:bookId">
+              <ReviewListPage/>
             </Route>
             <Route path="/checkout/:bookId">
               <BookCheckoutPage />
