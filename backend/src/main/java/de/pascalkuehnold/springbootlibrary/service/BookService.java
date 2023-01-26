9ce1a9b049b28a42bb641eb4696af7fccae8431c
PascalKuehnold <pascal.kuehnold@gmail.com>
2023-01-27 00:50:17 +0100

    added method to get the current loans of a user

--- backend/src/main/java/de/pascalkuehnold/springbootlibrary/service/BookService.java
+++ backend/src/main/java/de/pascalkuehnold/springbootlibrary/service/BookService.java
@@ -4,11 +4,17 @@ import de.pascalkuehnold.springbootlibrary.dao.BookRepository;
 import de.pascalkuehnold.springbootlibrary.dao.CheckoutRepository;
 import de.pascalkuehnold.springbootlibrary.entity.Book;
 import de.pascalkuehnold.springbootlibrary.entity.Checkout;
+import de.pascalkuehnold.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
@@ -24,12 +30,12 @@ public class BookService {
         this.checkoutRepository = checkoutRepository;
     }
 
-    public Book checkoutBook(String userEmail, Long bookId) throws Exception{
+    public Book checkoutBook(String userEmail, Long bookId) throws Exception {
         Optional<Book> book = bookRepository.findById(bookId);
 
         Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
 
-        if(!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0){
+        if (!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
             throw new Exception("Book doesn't exist or already checked out by user.");
         }
 
@@ -48,14 +54,48 @@ public class BookService {
         return book.get();
     }
 
-    public boolean checkoutBookByUser(String userEmail, Long bookId){
+    public boolean checkoutBookByUser(String userEmail, Long bookId) {
         Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
 
         return validateCheckout != null;
     }
 
-    public int currentLoandsCount(String userEmail){
+    public int currentLoandsCount(String userEmail) {
         return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
+
+    public List<ShelfCurrentLoansResponse> currentLoans(String userEmail) throws Exception {
+        List<ShelfCurrentLoansResponse> shelfCurrentLoansResponses = new ArrayList<>();
+
+        List<Checkout> checkoutList = checkoutRepository.findBooksByUserEmail(userEmail);
+
+        List<Long> bookIdList = new ArrayList<>();
+
+        for (Checkout i : checkoutList) {
+            bookIdList.add(i.getBookId());
+        }
+
+        List<Book> books = bookRepository.findBooksByBookIds(bookIdList);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        for (Book book : books) {
+            Optional<Checkout> checkout = checkoutList.stream()
+                    .filter(x -> x.getBookId() == book.getId()).findFirst();
+
+            if(checkout.isPresent()){
+                Date d1 = sdf.parse(checkout.get().getReturnDate());
+                Date d2 = sdf.parse(LocalDate.now().toString());
+
+                TimeUnit time = TimeUnit.DAYS;
+
+                long difference_In_Time = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+
+                shelfCurrentLoansResponses.add(new ShelfCurrentLoansResponse(book, (int) difference_In_Time));
+            }
+
+        }
+        return shelfCurrentLoansResponses;
     }
 
 }
